5672fe95e9bc02bf725f004fb9e871e838cf1126
avonkannewurff3 <avonkannewurff3@gatech.edu>
2025-03-07 18:24:53 -0500

    Clone from template

---
+++ source/main.lua
@@ -0,0 +1,18 @@
+-- Below is a small example program where you can move a circle
+-- around with the crank. You can delete everything in this file,
+-- but make sure to add back in a playdate.update function since
+-- one is required for every Playdate game!
+-- =============================================================
+
+-- Importing libraries used for drawCircleAtPoint and crankIndicator
+import "CoreLibs/graphics"
+import "CoreLibs/ui"
+
+-- Localizing commonly used globals
+local pd <const> = playdate
+local gfx <const> = playdate.graphics
+-- playdate.update function is required in every project!
+function playdate.update()
+    -- Clear screen
+    gfx.clear()
+end
